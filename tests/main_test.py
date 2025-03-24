f99a0e1e09ee78b057ce75c38f5c192e9d4e3390
Mikeinnet <misa32034@gmail.com>
2025-03-24 20:45:10 +0200

    Update test file import

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,9 @@
 import pytest
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from app import create_app
-#Tests
 
 @pytest.fixture()
 def app():
